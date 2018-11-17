112d8e3fd9cc5685819b899bd41f3f3893f20762
jariazavalverde <riazavalverde@gmail.com>
2018-11-17 18:45:30 +0100

    added string tokens

--- src/smtlib.pl
+++ src/smtlib.pl
@@ -9,6 +9,18 @@
 
 
 
+/**
+  * 
+  * Barrett, Clark, Aaron Stump, and Cesare Tinelli. "The smt-lib standard: Version 2.0."
+  * Proceedings of the 8th International Workshop on Satisfiability Modulo Theories (Edinburgh, England).
+  * Vol. 13. 2010.
+  * 
+  * http://smtlib.cs.uiowa.edu/papers/smt-lib-reference-v2.0-r12.09.09.pdf
+  * 
+  **/
+
+
+
 % WHITESPACES
 
 % A comment is any character sequence not contained within a string literal
@@ -55,3 +67,16 @@ token_binary(binary(Y)) --> ['#','b'], binary(X), {X \= [], atom_chars(Y, X)}.
 
 binary([X|Xs]) --> [X], {member(X, ['0','1'])}, !, binary(Xs).
 binary([]) --> [].
+
+% A <string> is any sequence of printable ASCII characters delimited by double quotes
+% (") and possibly containing the C-style escape sequences \" and \\, both of which are
+% treated as a single character—respectively " and \.  The first escape sequence allows
+% as usual the double quote character to appear within a string literal, the second allows
+% the backslash character to end a string literal.
+
+token_string(string(Y)) --> ['"'], quoted(X), ['"'], {atom_chars(Y, X)}.
+
+quoted([X|Xs]) --> [X], {X \= '\\', X \= '"'}, !, quoted(Xs).
+quoted(['"'|Xs]) --> ['\\','"'], !, quoted(Xs).
+quoted(['\\'|Xs]) --> ['\\','\\'], !, quoted(Xs).
+quoted([]) --> [].
