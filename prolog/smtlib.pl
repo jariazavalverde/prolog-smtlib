703c01844353e19e1576270f83386f25bb4b375e
jariazavalverde <riazavalverde@gmail.com>
2018-11-18 15:50:09 +0100

    minor refactoring

--- prolog/smtlib.pl
+++ prolog/smtlib.pl
@@ -57,41 +57,45 @@ whitespace --> [';'], comment, ['\n'].
 whitespaces --> whitespace, !, whitespaces.
 whitespaces --> [].
 
-% A <numeral> is the digit 0 or a non-empty sequence of digits not starting with 0.
-token_numeral(numeral(Y)) --> numeral([X|Xs]), {X \= '0', number_chars(Y, [X|Xs])}.
+lpar --> ['('], whitespaces.
+rpar --> [')'], whitespaces.
 
-numeral([X|Xs]) --> [X], {char_code(X, C), C >= 48, C =< 57}, !, numeral(Xs).
-numeral([]) --> [].
+% A <numeral> is the digit 0 or a non-empty sequence of digits not starting with 0.
+numeral(numeral(Y)) --> digits([X|Xs]), {(Xs = [] ; X \= '0'), number_chars(Y, [X|Xs])}, whitespaces.
+
+digits([X|Xs]) --> [X], {char_code(X, C), C >= 48, C =< 57}, !, digits(Xs).
+digits([]) --> [].
 
 % A <decimal> is a token of the form <numeral>.0*<numeral>.
-token_decimal(decimal(D)) -->
-    token_numeral(numeral(X)),
-    ['.'], numeral(Y),
-    {atom_chars(Z, ['.'|Y]),
-    atom_concat(X, Z, W),
-    atom_number(W, D)}.
+decimal(decimal(D)) -->
+    digits([X|Xs]),
+    {(Xs = [] ; X \= '0')},
+    ['.'], digits(Y),
+    {append([X|Xs], ['.'|Y], Z),
+    number_chars(D, Z)},
+    whitespaces.
 
 % A <hexadecimal> is a non-empty case-insensitive sequence of digits and letters
 % from A to F preceded by the (case sensitive) characters #x.
-token_hexadecimal(hexadecimal(Y)) --> ['#','x'], hexadecimal(X), {X \= [], atom_chars(Y, X)}.
+hexadecimal(hexadecimal(Y)) --> ['#','x'], hexadecimal_digits(X), {X \= [], atom_chars(Y, X)}, whitespaces.
 
-hexadecimal([X|Xs]) --> [X],
+hexadecimal_digits([X|Xs]) --> [X],
     {member(X, ['0','1','2','3','4','5','6','7','8','9',a,b,c,d,e,f,'A','B','C','D','E','F'])}, !,
-    hexadecimal(Xs).
-hexadecimal([]) --> [].
+    hexadecimal_digits(Xs).
+hexadecimal_digits([]) --> [].
 
 % A <binary> is a non-empty sequence of the characters 0 and 1 preceded by the characters #b.
-token_binary(binary(Y)) --> ['#','b'], binary(X), {X \= [], atom_chars(Y, X)}.
+binary(binary(Y)) --> ['#','b'], binary_digits(X), {X \= [], atom_chars(Y, X)}, whitespaces.
 
-binary([X|Xs]) --> [X], {member(X, ['0','1'])}, !, binary(Xs).
-binary([]) --> [].
+binary_digits([X|Xs]) --> [X], {member(X, ['0','1'])}, !, binary_digits(Xs).
+binary_digits([]) --> [].
 
 % A <string> is any sequence of printable ASCII characters delimited by double quotes
 % (") and possibly containing the C-style escape sequences \" and \\, both of which are
 % treated as a single character—respectively " and \.  The first escape sequence allows
 % as usual the double quote character to appear within a string literal, the second allows
 % the backslash character to end a string literal.
-token_string(string(Y)) --> ['"'], quoted(X), ['"'], {atom_chars(Y, X)}.
+string(string(Y)) --> ['"'], quoted(X), ['"'], {atom_chars(Y, X)}, whitespaces.
 
 quoted([X|Xs]) --> [X], {X \= '\\', X \= '"'}, !, quoted(Xs).
 quoted(['"'|Xs]) --> ['\\','"'], !, quoted(Xs).
@@ -108,41 +112,43 @@ reserved_word(X) :- member(X, [
     'get-proof', 'get-unsat-core', 'get-value', 'get-assignment', exit
 ]).
 
-token_reserved(reserved(Y)) -->
-    simple_symbol([X|Xs]),
+reserved_word(reserved(Y)) -->
+    symbol_chars([X|Xs]),
     {atom_chars(Y, [X|Xs]),
-    reserved_word(Y)}.
+    reserved_word(Y)},
+    whitespaces.
 
 % A <symbol> is either a simple symbol or a quoted symbol. The former is any non-empty
 % sequence of letters, digits and the characters ~ ! @ $ % ^ & * _ - + = < > . ? / that
 % does not start with a digit and is not a reserved word. The latter is any sequence of
 % printable ASCII characters (including space, tab, and line-breaking characters) except
 % for the backslash character \, that starts and ends with | and does not otherwise contain |.
-token_symbol(X) --> token_simple_symbol(X).
-token_symbol(X) --> token_quoted_symbol(X).
+symbol(X) --> simple_symbol(X).
+symbol(X) --> quoted_symbol(X).
 
-token_simple_symbol(symbol(Y)) -->
-    simple_symbol([X|Xs]),
+simple_symbol(symbol(Y)) -->
+    symbol_chars([X|Xs]),
     {\+member(X, ['0','1','2','3','4','5','6','7','8','9']),
     atom_chars(Y, [X|Xs]),
-    \+reserved_word(Y)}.
+    \+reserved_word(Y)},
+    whitespaces.
 
-simple_symbol([X|Xs]) --> [X],
+symbol_chars([X|Xs]) --> [X],
     {member(X, ['~','!','@','$','%','^','&','*','_','-','+','=','<','>','.','?','/']) ;
     (char_code(X, C), C >= 48, C =< 57) ; 
     (char_code(X, C), C >= 97, C =< 122) ; 
     (char_code(X, C), C >= 65, C =< 90)}, !,
-    simple_symbol(Xs).
-simple_symbol([]) --> [].
+    symbol_chars(Xs).
+symbol_chars([]) --> [].
 
-token_quoted_symbol(symbol(Y)) --> ['|'], quoted_symbol(X), ['|'], {atom_chars(Y, X)}.
+quoted_symbol(symbol(Y)) --> ['|'], quoted_symbol_chars(X), ['|'], {atom_chars(Y, X)}, whitespaces.
 
-quoted_symbol([X|Xs]) --> [X], {X \= '|', X \= '\\'}, quoted_symbol(Xs).
-quoted_symbol([]) --> [].
+quoted_symbol_chars([X|Xs]) --> [X], {X \= '|', X \= '\\'}, quoted_symbol_chars(Xs).
+quoted_symbol_chars([]) --> [].
 
 % A <keyword> is a non-empty sequence of letters, digits, and the characters
 % ~ ! @ $ % ^ & * _ - + = < > . ? / preceded by the character :.
-token_keyword(keyword(Y)) --> [':'], simple_symbol(X), {atom_chars(Y, X)}.
+keyword(keyword(Y)) --> [':'], symbol_chars(X), {X \= [], atom_chars(Y, X)}, whitespaces.
 
 
 
@@ -151,17 +157,17 @@ token_keyword(keyword(Y)) --> [':'], simple_symbol(X), {atom_chars(Y, X)}.
 % An S-expression is either a non-parenthesis token or a (possibly  empty) sequence of
 % S-expressions enclosed in parentheses. Every syntactic category of the SMT-LIB language
 % is a specialization of the category <s-expr> defined by the production rules below.
-spec_constant(X) --> token_decimal(X), !.
-spec_constant(X) --> token_numeral(X), !.
-spec_constant(X) --> token_hexadecimal(X), !.
-spec_constant(X) --> token_binary(X), !.
-spec_constant(X) --> token_string(X), !.
+spec_constant(X) --> decimal(X), !.
+spec_constant(X) --> numeral(X), !.
+spec_constant(X) --> hexadecimal(X), !.
+spec_constant(X) --> binary(X), !.
+spec_constant(X) --> string(X), !.
 
 s_expr(X) --> whitespaces, s_expr2(X).
-s_expr2(X) --> spec_constant(X), !, whitespaces.
-s_expr2(X) --> token_symbol(X), !, whitespaces.
-s_expr2(X) --> token_keyword(X), !, whitespaces.
-s_expr2(X) --> ['('], whitespaces, s_exprs(X), [')'], whitespaces.
+s_expr2(X) --> spec_constant(X), !.
+s_expr2(X) --> symbol(X), !.
+s_expr2(X) --> keyword(X), !.
+s_expr2(X) --> lpar, s_exprs(X), rpar.
 
 s_exprs([X|Xs]) --> s_expr2(X), !, s_exprs(Xs).
 s_exprs([]) --> [].
@@ -172,10 +178,10 @@ s_exprs([]) --> [].
 
 % Indexed identifiers are defined more systematically as the application of the reserved
 % word _ to a symbol and one or more indices, given by numerals.
-identifier(identifier(Xs)) --> ['('], whitespaces, ['_'], whitespaces, numerals(Xs), {Xs \= []}, [')'], whitespaces.
-identifier(identifier(X)) --> token_symbol(X), whitespaces.
+identifier(identifier(Xs)) --> lpar, ['_'], whitespaces, numerals(Xs), {Xs \= []}, rpar.
+identifier(identifier(X)) --> symbol(X).
 
-numerals([X|Xs]) --> numeral(X), !, whitespaces, numerals(Xs).
+numerals([X|Xs]) --> numeral(X), !, numerals(Xs).
 numerals([]) --> [].
 
 
@@ -185,12 +191,12 @@ numerals([]) --> [].
 % These are generally pairs consisting of an attribute name and an associated value,
 % although attributes with no value are also allowed.
 
-attribute_value(X) --> spec_constant(X), !, whitespaces.
-attribute_value(X) --> token_symbol(X), !, whitespaces.
-attribute_value(Xs) --> ['('], whitespaces, s_exprs(Xs), [')'], whitespaces.
+attribute_value(X) --> spec_constant(X), !.
+attribute_value(X) --> symbol(X), !.
+attribute_value(Xs) --> lpar, s_exprs(Xs), rpar.
 
-attribute(attr(X,Xs)) --> token_keyword(X), whitespaces, attribute_value(Xs), !, whitespaces.
-attribute(attr(X)) --> token_keyword(X), whitespaces.
+attribute(attr(X,Xs)) --> keyword(X), attribute_value(Xs), !.
+attribute(attr(X)) --> keyword(X).
 
 attributes([X|Xs]) --> attribute(X), !, attributes(Xs).
 attributes([]) --> [].
@@ -201,8 +207,8 @@ attributes([]) --> [].
 % A sort symbol can be either the distinguished symbol Bool or any <identifier>. A sort
 % parameter can be any <symbol> (which in turn, is an <identifier>).
 
-sort(sort(X, Xs)) --> ['('], whitespaces, identifier(X), whitespaces, sorts(Xs), {Xs \= []}, [')'], whitespaces.
-sort(sort(X, [])) --> identifier(X), whitespaces.
+sort(sort(X, Xs)) --> lpar, identifier(X), sorts(Xs), {Xs \= []}, rpar.
+sort(sort(X, [])) --> identifier(X).
 
 sorts([X|Xs]) --> sort(X), sorts(Xs).
 sorts([]) --> [].
@@ -217,22 +223,34 @@ sorts([]) --> [].
 % exists--and an annotation operator--the reserved word !.
 
 qual_identifier(qualified(X)) --> identifier(X).
-qual_identifier(qualified(X,Y)) --> ['('], whitespaces, [a,s], whitespaces, identifier(X), sort(Y), [')'], whitespaces.
+qual_identifier(qualified(X,Y)) --> lpar, [a,s], whitespaces, identifier(X), sort(Y), rpar.
 
-var_binding(binding(X,Y)) --> ['('], whitespaces, token_symbol(X), whitespaces, term(Y), [')'], whitespaces.
+var_binding(binding(X,Y)) --> lpar, symbol(X), term(Y), rpar.
 var_bindings([X|Xs]) --> var_binding(X), !, var_bindings(Xs).
 var_bindings([]) --> [].
 
-sorted_var(var(X,Y)) --> ['('], whitespaces, token_symbol(X), whitespaces, sort(Y), [')'], whitespaces.
+sorted_var(var(X,Y)) --> lpar, symbol(X), sort(Y), rpar.
 sorted_vars([X|Xs]) --> sorted_var(X), !, sorted_vars(Xs).
 sorted_vars([]) --> [].
 
-term(term(X)) --> spec_constant(X), whitespaces.
+term(term(X)) --> spec_constant(X).
 term(term(X)) --> qual_identifier(X).
-term(term(X,Xs)) --> ['('], whitespaces, qual_identifier(X), whitespaces, terms(Xs), {Xs \= []}, [')'], whitespaces.
-term(term(let, Xs, X)) --> ['('], whitespaces, token_reserved(reserved(let)), whitespaces, ['('], whitespaces, var_bindings(Xs), {Xs \= []}, [')'], whitespaces, term(X), [')'], whitespaces.
-term(term(Y, Xs, X)) --> ['('], whitespaces, token_reserved(reserved(Y)), {member(Y, [forall, exists])}, whitespaces, ['('], whitespaces, sorted_vars(Xs), {Xs \= []}, [')'], whitespaces, term(X), [')'], whitespaces.
-term(term('!', Xs)) --> ['('], whitespaces, token_reserved(reserved('!')), whitespaces, attributes(Xs), {Xs \= []}, [')'], whitespaces.
+term(term(X,Xs)) --> lpar, qual_identifier(X), terms(Xs), {Xs \= []}, rpar.
+term(term(reserved(let), Xs, X)) --> lpar, reserved_word(reserved(let)), lpar, var_bindings(Xs), {Xs \= []}, rpar, term(X), rpar.
+term(term(reserved(Y), Xs, X)) --> lpar, reserved_word(reserved(Y)), {member(Y, [forall, exists])}, lpar, sorted_vars(Xs), {Xs \= []}, rpar, term(X), rpar.
+term(term(reserved('!'), Xs)) --> lpar, reserved_word(reserved('!')), attributes(Xs), {Xs \= []}, rpar.
 
 terms([X|Xs]) --> term(X), !, terms(Xs).
 terms([]) --> [].
+
+
+
+% THEORY DECLARATIONS
+
+% The syntax of theory declarations follows an attribute-value-based format. A theory
+% declaration consists of a theory name and a list of <attribute> elements. Theory attributes
+% with the following predefined keywords have a prescribed usage and semantics: :definition,
+% :funs, :funs-description, :notes, :sorts, :sorts-description, and :values. Additionally, a
+% theory declaration can contain any number of user-defined attributes.
+
+sort_symbol_decl(sort_symbol_decl(X,Y,Z)) --> lpar, identifier(X), numeral(Y), attributes(Z), rpar.
