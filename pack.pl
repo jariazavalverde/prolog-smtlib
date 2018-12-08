c033444fb288d022b42f3205feebffc781a129d0
jariazavalverde <riazavalverde@gmail.com>
2018-12-08 23:26:46 +0100

    released v0.0.5

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(smtlib).
 title('SMT-LIB parser for SWI-Prolog').
-version('0.0.3').
+version('0.0.5').
 author('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
 maintainer('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
 packager('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
